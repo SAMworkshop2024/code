5c60d4bfd3baab312713831ed0c27f8d80b04727
Martin Jucker <coding@martinjucker.com>
2024-05-09 17:05:19 +1000

    Create functions.py
    This is an empty file to serve as collector for commonly useful functions.

---
+++ functions.py
@@ -0,0 +1,5 @@
+# this is where we can add functions which might be used inside more than one analysis script.
+#  every script can then import this file via 
+#  import .functions as fc
+#  and access a function with 
+#  fc.function_name()
