a622438052b05b6ed05213bfd9555b9727104fa8
Martin Jucker <coding@martinjucker.com>
2024-06-14 20:47:20 +1000

    updated 300hPa jet plot

--- sam_jets.py
+++ sam_jets.py
@@ -2,12 +2,13 @@ import xarray as xr
 from aostools import climate as ac
 import numpy as np
 import scipy.signal as sg
+import os
 
 level = 300
 proj = 'Robinson'
-latslice = {'lat':slice(-90,20)}
+latslice = {'lat':slice(-90,0)}
 
-threshold = 1.0
+threshold = 0.0
 
 plot_seasons = ['DJF','JJA']
 
@@ -29,12 +30,18 @@ def ReadERA(ds):
     ds = ac.StandardGrid(ds,rename=True)
     return ds.sel(pres=level).sel(latslice)
 
-
-u = xr.open_mfdataset(era_base+'pressure-levels/monthly-averaged/u/*/*',preprocess=ReadERA)
-u = u.sel(time=slice(sam.time[0],sam.time[-1])).u.load()
-if detrend:
-    u = u.groupby('time.season').map(detrend_x)
-
+ufile = 'u_monthly.nc'
+if os.path.isfile(ufile):
+    print('reading',ufile)
+    u = xr.open_dataarray(ufile)
+else:
+    u = xr.open_mfdataset(era_base+'pressure-levels/monthly-averaged/u/*/*',preprocess=ReadERA)
+    u = u.sel(time=slice(sam.time[0],sam.time[-1])).u.load()
+    if detrend:
+        u = u.groupby('time.season').map(detrend_x)
+    u.to_netcdf('u_monthly.nc')
+    print(ufile)
+    
 MAMJJA = (sam['time.month']>=3)*(sam['time.month']<=8)
 SONDJF = np.invert(MAMJJA)
 
@@ -73,8 +80,9 @@ fltr = filtr2
 #    nrows = nrows//2
     
 ncols = 3
+nlevs = 10
 fig,axs,transf = ac.Projection(proj,ncols=ncols,nrows=nrows,kw_args={'central_longitude':155})
-fig.set_figheight(nrows*2)
+fig.set_figheight(nrows*1.1)
 fig.set_figwidth(ncols*4*1.2)
 
 keys = sorted(list(fltr.keys()))
@@ -88,33 +96,35 @@ if overlap:
         key = '{0}, {1}'.format(season,polarity)
         ax = axs[a][0]
         upos = u.isel(time=fltr[key]).mean('time')
-        cl = upos.plot.contourf(levels=11,ax=ax,vmin=10,vmax=40,cmap='Reds',add_colorbar=False,**transf) 
+        cl = upos.plot.contourf(levels=nlevs,ax=ax,vmin=10,vmax=40,cmap='Reds',add_colorbar=False,**transf) 
         ax.set_title(key)
         ax.coastlines()  
         key = key.replace('>','<').replace('{0}'.format(threshold),'{0}'.format(-threshold))
         ax = axs[a][1]
         uneg = u.isel(time=fltr[key]).mean('time')
-        cl = uneg.plot.contourf(levels=11,ax=ax,vmin=10,vmax=40,cmap='Reds',add_colorbar=False,**transf)
+        cl = uneg.plot.contourf(levels=nlevs,ax=ax,vmin=10,vmax=40,cmap='Reds',add_colorbar=False,**transf)
         ax.set_title(key)
         ax.coastlines()  
         ax = axs[a][2]
-        cd = (upos-uneg).plot(levels=11,ax=ax,cmap='RdBu_r',vmin=-10,add_colorbar=False,**transf) 
-        ax.set_title('difference')
-        ax.coastlines()  
+        cd = (upos-uneg).plot.contourf(levels=nlevs,ax=ax,vmin=-6,cmap='RdBu_r',extend='both',add_colorbar=False,**transf) 
+        ax.set_title('{0}, difference'.format(season))
+        ax.coastlines()
+        # there's a bug where the last axes show the whole globe
+        axs[a][2].set_ylim(axs[0][0].get_ylim())
 else:
     for a,ax in enumerate(axs.flat):
         key = keys[a]
         season = key.split(',')[0]
         if season not in plot_seasons:
             continue
-        cl = u.isel(time=fltr[key]).mean('time').plot(ax=ax,vmin=10,vmax=50,cmap='Reds',add_colorbar=False,**transf)
+        cl = u.isel(time=fltr[key]).mean('time').plot.contourf(levels=11,ax=ax,vmin=10,vmax=50,cmap='Reds',add_colorbar=False,**transf)
         ax.set_title(key)
         ax.coastlines()
 if overlap:
-    ac.AddColorbar(fig,axs,cd,shrink=0.8,cbar_args={'label':'u300 [m/s]'})
-ac.AddColorbar(fig,axs,cl,shrink=0.8,cbar_args={'label':'u300 [m/s]','location':'left'})
+    ac.AddColorbar(fig,axs,cd,shrink=0.8,cbar_args={'label':'zonal wind difference [ms-1]'})
+ac.AddColorbar(fig,axs,cl,shrink=0.8,cbar_args={'label':'zonal wind [ms-1]','location':'left'})
 ac.AddPanelLabels(axs,'lower left')
-fig.suptitle('{0}hPa zonal wind, ERA5 1979-2023'.format(level))
+fig.suptitle('Seasonal {0}hPa zonal wind by SAM phase'.format(level))
 outFile = 'SAM_u{1}_SAM{0}.pdf'.format(threshold,level)
 if detrend:
     outFile = outFile.replace('.pdf','_detrend.pdf')
