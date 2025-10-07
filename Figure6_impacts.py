467535e5ca4f825c84b15ff27eb815e45d2117b3
Martin Jucker <coding@martinjucker.com>
2025-10-07 13:20:14 +1100

    trying to recreate final submitted Figure 6, but failing along the way

--- Figure6_impacts.py
+++ Figure6_impacts.py
@@ -2,7 +2,10 @@ import xarray as xr
 
 datadir = '../data/'
 
+lat_lim = -40
+
 dset=xr.open_dataset(datadir+'SAT_anomaly.nc')
+dset = dset.sel(latitude = slice(lat_lim,-90))
 temp=dset.__xarray_dataarray_variable__
 
 years=xr.cftime_range(start='1979-01-01', end='2023-12-31', freq='Y')
@@ -10,6 +13,7 @@ years=xr.cftime_range(start='1979-01-01', end='2023-12-31', freq='Y')
 # Select months of interest for NDJFMA and MJJASO
 months_ndjfma = [11, 12, 1, 2, 3, 4]
 months_mjjaso = [5, 6, 7, 8, 9, 10]
+
 
 # Select data for NDJFMA and MJJASO
 temp_ndjfma1 = temp.sel(time=temp['time.month'].isin(months_ndjfma))
@@ -30,6 +34,7 @@ for i in range(len(years.year)):
 temp_mjjaso=temp_mjjaso[1:].assign_coords(years=years)
 
 dset=xr.open_dataset(datadir+'precip_anomaly.nc')
+dset = dset.sel(latitude = slice(lat_lim,-90))
 precip=dset.__xarray_dataarray_variable__
 
 years=xr.cftime_range(start='1979-01-01', end='2023-12-31', freq='Y')
@@ -58,7 +63,7 @@ precip_mjjaso=precip_mjjaso[1:].assign_coords(years=years)
 
 import cartopy.crs as ccrs
 
-dset=xr.open_dataset('/scratch/w97/rr8442/seaice_conc.nc')
+dset=xr.open_dataset(datadir+'seaice_conc.nc')
 sea_ice_extent = dset.cdr_seaice_conc_monthly.sel(tdim=slice('1979-01-01','2023-12-01')).values 
 data_proj = ccrs.epsg(dset.projection.attrs["srid"].split("::")[-1])
 xgrid = dset.xgrid.values
@@ -86,6 +91,7 @@ for i in range(len(years.year)):
 sic_mjjaso=sic_mjjaso[1:].assign_coords(years=years)
 
 dset=xr.open_dataset(datadir+'u850_anomaly.nc')
+dset = dset.sel(latitude = slice(lat_lim,-90))
 wind=dset.__xarray_dataarray_variable__
 months_of_interest = [11,12,1,2,3,4] 
 wind_ndfma = wind.sel(time=wind['time.month'].isin(months_of_interest))
@@ -111,6 +117,7 @@ for i in range(len(years.year)):
 wind_mjjaso=wind_mjjaso[1:].assign_coords(years=years)
 
 dset=xr.open_dataset(datadir+'v850_anomaly.nc')
+dset = dset.sel(latitude = slice(lat_lim,-90))
 vwind=dset.__xarray_dataarray_variable__
 months_of_interest = [11,12,1,2,3,4] 
 vwind_ndfma = vwind.sel(time=vwind['time.month'].isin(months_of_interest))
@@ -187,48 +194,78 @@ def masking(px):
     
     #apply mask: very inefficient way
     px_mask[px_mask.mask] = np.nan
-    return(px_mask,)
+    return (px_mask,)
+
+# mj different way - currently doesn't work when trying to apply .where() below
+def masking(px):
+    M = ((abs(px)>=0.05))
+    return px.where(M)
 
 vwind_djf.slope=masking(vwind_djf.slope)
 wind_djf.slope=masking(wind_djf.slope)
 vwind_jja.slope=masking(vwind_jja.slope)
 wind_jja.slope=masking(wind_jja.slope)
 
+
+import cmaps
 import cartopy.crs as ccrs
 import sacpy.Map
 from matplotlib import rc
-rc('text', usetex=True)
-plt.rc('axes', linewidth=1.5)
-plt.rc('font', weight='bold',size=15)
-rc('font',**{'family':'serif','serif':['Times']})
+#rc('text', usetex=True)
+#plt.rc('axes', linewidth=1.5)
+#plt.rc('font', weight='bold',size=15)
+#rc('font',**{'family':'serif','serif':['Times']})
+from matplotlib import path as mpath
+from cartopy import feature as cfeature
 
+proj = ccrs.SouthPolarStereo()
+
+theta = np.linspace(0, 2*np.pi, 100)
+center, radius = [0.5, 0.5], .5
+verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+circle = mpath.Path(verts * radius + center)
+center_new, radius_new = [0.5, 0.5], 0.60  # New center and radius
+circle_new = mpath.Path(verts * radius_new + center_new)
 
 dset=xr.open_dataset(datadir+'SAT_anomaly.nc')
+dset = dset.sel(latitude = slice(lat_lim,-90))
 temp=dset.__xarray_dataarray_variable__
 
 lon ,lat = temp.longitude , temp.latitude
 fig = plt.figure(figsize=[13,13],dpi=100)
-ax = fig.add_subplot(321,projection=ccrs.PlateCarree())
-m = ax.scontourf(lon,lat,temp_djf.slope.where(temp_djf.p_value<0.1),cmap=cmaps.BlueWhiteOrangeRed ,extend='both')
-ax.init_map(stepx=50,smally=2.5)
+
+
+ax = fig.add_subplot(321,projection=proj)
+#ax.set_boundary(circle, transform=ax.transAxes)
+m = ax.contourf(lon,lat,temp_djf.slope.where(temp_djf.p_value<0.1),cmap=cmaps.BlueWhiteOrangeRed ,vmin=-0.8,vmax=0.8,extend='both',transform=ccrs.PlateCarree())
+ax.set_boundary(circle_new, transform=ax.transAxes)
+ax.coastlines()
+#ax.init_map(stepx=50,smally=2.5)
 ax.set_title('NDJFMA',)
 
-ax1 = fig.add_subplot(322,projection=ccrs.PlateCarree())
-m = ax1.scontourf(lon,lat,temp_jja.slope.where(temp_jja.p_value<0.1),cmap=cmaps.BlueWhiteOrangeRed ,extend='both',)
-ax1.init_map(stepx=50,smally=2.5)
+ax1 = fig.add_subplot(322,projection=proj)
+#ax1.set_boundary(circle, transform=ax1.transAxes)
+m = ax1.contourf(lon,lat,temp_jja.slope.where(temp_jja.p_value<0.1),cmap=cmaps.BlueWhiteOrangeRed ,vmin=-0.8,vmax=0.8,extend='both',transform=ccrs.PlateCarree())
+ax1.coastlines()
+ax1.set_boundary(circle_new, transform=ax1.transAxes)
+#ax1.init_map(stepx=50,smally=2.5)
 ax1.set_title('MJJASO',)
 
-ax2 = fig.add_subplot(323,projection=ccrs.PlateCarree())
-m1 = ax2.scontourf(lon,lat,precip_djf.slope.where(precip_djf.p_value<0.1),cmap=cmaps.precip_diff_12lev,extend='both')
-ax2.squiver(lon,lat,wind_djf.slope.where(wind_djf.p_value<0.1),vwind_djf.slope.where(vwind_djf.p_value<0.1),stepx=30,stepy=15,headwidth=2,headaxislength=8,width=.0055,minlength=2)
+ax2 = fig.add_subplot(323,projection=proj)
+#ax2.set_boundary(circle, transform=ax2.transAxes)
+m1 = ax2.contourf(lon,lat,1e6*precip_djf.slope.where(precip_djf.p_value<0.1),cmap=cmaps.precip_diff_12lev,vmin=-4,vmax=4,extend='both',transform=ccrs.PlateCarree())
+ax2.squiver(lon,lat,wind_djf.slope.where(wind_djf.p_value<0.1),vwind_djf.slope.where(vwind_djf.p_value<0.1),stepx=30,stepy=15,headwidth=2,headaxislength=8,width=.0055,minlength=2,transform=ccrs.PlateCarree())
+ax2.set_boundary(circle_new, transform=ax2.transAxes)
 ax2.coastlines()
-ax2.init_map(smally=2.5)
+#ax2.init_map(smally=2.5)
 #stepx=40,stepy=20,headwidth=2,headaxislength=3,width=.006,minlength=2
-ax3 = fig.add_subplot(324,projection=ccrs.PlateCarree())
-m1 = ax3.scontourf(lon,lat,precip_jja.slope.where(precip_jja.p_value<0.1),cmap=cmaps.precip_diff_12lev,extend='both')
+ax3 = fig.add_subplot(324,projection=proj)
+#ax3.set_boundary(circle, transform=ax3.transAxes)
+m1 = ax3.contourf(lon,lat,1e6*precip_jja.slope.where(precip_jja.p_value<0.1),cmap=cmaps.precip_diff_12lev,vmin=-4,vmax=4,extend='both',transform=ccrs.PlateCarree())
 ax3.squiver(lon,lat,wind_jja.slope.where(wind_jja.p_value<0.1),vwind_jja.slope.where(vwind_jja.p_value<0.1),stepx=30,stepy=15,headwidth=2,headaxislength=8,width=.0055,minlength=2)
+ax3.set_boundary(circle_new, transform=ax3.transAxes)
 ax3.coastlines()
-ax3.init_map(smally=2.5)
+#ax3.init_map(smally=2.5)
 
 from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
@@ -241,36 +278,24 @@ cmap=cmaps.BlueWhiteOrangeRed
 norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
 
 
-ax4 = fig.add_subplot(325,projection=ccrs.Orthographic(180, -90))
-theta = np.linspace(0, 2*np.pi, 100)
-center, radius = [0.5, 0.5], .5
-verts = np.vstack([np.sin(theta), np.cos(theta)]).T
-circle = mpath.Path(verts * radius + center)
+ax4 = fig.add_subplot(325,projection=proj)
 ax4.set_boundary(circle, transform=ax4.transAxes)
-ax4.add_feature(cfeature.LAND, edgecolor='black', facecolor='lightgray')
-ax4.add_feature(cfeature.COASTLINE, edgecolor='black')
-m2=ax4.pcolormesh(xgrid, ygrid, sic_djf.slope.where(sic_djf.p_value<0.1),transform=data_proj,norm=norm,cmap=cmaps.BlueWhiteOrangeRed,)
+#ax4.add_feature(cfeature.LAND, edgecolor='black', facecolor='lightgray')
+#ax4.add_feature(cfeature.COASTLINE, edgecolor='black')
+m2=ax4.scontourf(xgrid, ygrid, 100*sic_djf.slope.where(sic_djf.p_value<0.1),transform=data_proj,vmin=-8,vmax=8,extend='both',cmap='PiYG_r')#cmaps.BlueWhiteOrangeRed,)
 ax4.coastlines()
-center_new, radius_new = [0.5, 0.5], 0.60  # New center and radius
-circle_new = mpath.Path(verts * radius_new + center_new)
 ax4.set_boundary(circle_new, transform=ax4.transAxes)
-gl = ax4.gridlines(draw_labels=False, xlocs=None, ylocs=None)
+#gl = ax4.gridlines(draw_labels=False, xlocs=None, ylocs=None)
 
 
-ax5 = fig.add_subplot(326,projection=ccrs.Orthographic(180, -90))
-theta = np.linspace(0, 2*np.pi, 100)
-center, radius = [0.5, 0.5], .5
-verts = np.vstack([np.sin(theta), np.cos(theta)]).T
-circle = mpath.Path(verts * radius + center)
+ax5 = fig.add_subplot(326,projection=proj)
 ax5.set_boundary(circle, transform=ax5.transAxes)
-ax5.add_feature(cfeature.LAND, edgecolor='black', facecolor='lightgray')
-ax5.add_feature(cfeature.COASTLINE, edgecolor='black')
-m2=ax5.pcolormesh(xgrid, ygrid, sic_jja.slope.where(sic_jja.p_value<0.1),transform=data_proj,norm=norm,cmap=cmaps.BlueWhiteOrangeRed,)
+#ax5.add_feature(cfeature.LAND, edgecolor='black', facecolor='lightgray')
+#ax5.add_feature(cfeature.COASTLINE, edgecolor='black')
+m2=ax5.scontourf(xgrid, ygrid, 100*sic_jja.slope.where(sic_jja.p_value<0.1),transform=data_proj,vmin=-8,vmax=8,extend='both',cmap='PiYG_r')
 ax5.coastlines()
-center_new, radius_new = [0.5, 0.5], 0.60  # New center and radius
-circle_new = mpath.Path(verts * radius_new + center_new)
 ax5.set_boundary(circle_new, transform=ax5.transAxes)
-gl = ax5.gridlines(draw_labels=False, xlocs=None, ylocs=None)
+#gl = ax5.gridlines(draw_labels=False, xlocs=None, ylocs=None)
 
 
 
