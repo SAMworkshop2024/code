015188fa7bcd78fc380532c2653c101a8f1a6f98
Valentina Ortiz Guzman <v.ortiz@usnw.edu.au>
2025-10-16 06:13:17 +1100

    Edits to this block to produce the top panel of Fig.3 as in the manuscript submitted

--- Fig3abc_MSLP.py
+++ Fig3abc_MSLP.py
@@ -45,6 +45,7 @@ corr_symm.name = 'Symmetric'
 
 # Merge all into one Dataset
 corrmap = xr.merge([corr, corr_symm, corr_asymm])
+corrmap = ac.CloseGlobe(corrmap)
 
 ## Plot components
 
@@ -54,47 +55,59 @@ import matplotlib.path as mpath
 # to make circular map only (no square box)
 theta = np.linspace(0, 2*np.pi, 100)
 map_circle = mpath.Path(np.vstack([np.sin(theta), np.cos(theta)]).T * 0.5 + [0.5, 0.5])
-
+vmin1 = -0.6
+vmax1 = 0.6
+vmin2 = -0.3
+vmax2 = 0.3
+clevs1 = 16 
+clevs2 = 16 
 transf = ccrs.PlateCarree()
-#clevs = 19
-clevs1 = np.linspace(-0.6, 0.6, 19)
-clevs2 = np.linspace(-0.3, 0.3, 19)
+fs = 18
+
 figArgs = {'transform':transf, 'extend':'both', 'add_colorbar':False}
-fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(5*3,5), subplot_kw={'projection': ccrs.SouthPolarStereo()})
-#fig.suptitle(f'Correlation between MSLP and SAM Index, monthly ERA5 {period.start}-{period.stop}')
+fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(6*3,6), subplot_kw={'projection': ccrs.SouthPolarStereo()})
+
+# titles for panels
+titles = ['Total SAM and MSLP corr.', 'Symmetric SAM and MSLP corr.', 'Asymmetric SAM and MSLP corr.']
 
 # total
-cf1 = corrmap['Total'].plot.contourf(ax=axs[0], levels=clevs1, **figArgs)
-axs[0].set_title(corrmap['Total'].name)
+cf1 = corrmap['Total'].plot.contourf(ax=axs[0], levels=clevs1, vmin=vmin1, **figArgs)
+ttl_a = titles[0]
+axs[0].set_title(r'$\bf{a\ }$' + f' {ttl_a}', fontsize=fs, loc='center')
+
 # symmetric
-cf1 = corrmap['Symmetric'].plot.contourf(ax=axs[1], levels=clevs1, **figArgs)
-axs[1].set_title(corrmap['Symmetric'].name)
+cf1 = corrmap['Symmetric'].plot.contourf(ax=axs[1], levels=clevs1, vmin=vmin1, **figArgs)
+ttl_b = titles[1]
+axs[1].set_title(r'$\bf{b\ }$' + f' {ttl_b}', fontsize=fs, loc='center')
+
 # asymmetric separately
-cf3 = corrmap['Asymmetric'].plot.contourf(ax=axs[2], levels=clevs2, **figArgs) 
-axs[2].set_title(corrmap['Asymmetric'].name)
+cf3 = corrmap['Asymmetric'].plot.contourf(ax=axs[2], levels=clevs2, vmin=vmin2, **figArgs) 
+ttl_c = titles[2]
+axs[2].set_title(r'$\bf{c\ }$' + f' {ttl_c}', fontsize=fs, loc='center')
 
 for ax in axs.flatten():
     ax.set_extent([-180, 180, -90, -20], ccrs.PlateCarree())
     ax.set_boundary(map_circle, transform=ax.transAxes)
     ax.coastlines()
-    ax.coastlines()
     
-# Add panel labels    
-for a,ax in enumerate(axs):
-    ax.text(x=0.01, y=1.06, s=ascii_lowercase[a],weight='bold', fontsize=20,transform=ax.transAxes)
-    
-cbarArgs = {'orientation':'horizontal', 'extend':'both', 'fraction':0.05, 'pad':0.05}
+cbarArgs1 = {'orientation':'horizontal', 'extend':'both', 'fraction':0.06, 'pad':0.05}
+cbarArgs2 = {'orientation':'horizontal', 'extend':'both', 'fraction':0.06, 'pad':0.05}
 
 # colorbar 1
-cb1 = fig.colorbar(cf1, ax=axs[:2], **cbarArgs)
-cb1.ax.locator_params(nbins=8)
+cb1 = fig.colorbar(cf1, ax=axs[:2], **cbarArgs1)
+#cb1.ax.locator_params(nbins=8)
+cb1.ax.tick_params(which='major', labelsize=14, length=7)
+ticks1 = [-0.6, -0.36, -0.12, 0.12, 0.36, 0.6]
+cb1.set_ticks(ticks1)
 
 # colorbar 2
-cb3 = fig.colorbar(cf3, ax=axs[2], **cbarArgs)
-cb3.ax.locator_params(nbins=8)
-
+cb3 = fig.colorbar(cf3, ax=axs[2], **cbarArgs2)
+cb3.ax.locator_params(nbins=6)
+cb3.ax.tick_params(which='major', labelsize=14, length=7)
+ticks3 = [-0.3, -0.18, -0.06, 0.06, 0.18, 0.3]
+cb3.set_ticks(ticks3)
 
 # change here path/name
-figFile = 'Fig3a.pdf'
+figFile = 'Fig3abc.pdf'
 fig.savefig(figFile, bbox_inches='tight', facecolor='white', transparent=False)
 print(figFile)
